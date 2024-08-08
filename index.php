ebff005ddfc76bfc06a0505b007abf4c147ccea5
Antonio Campello <100059926+antoniocampello@users.noreply.github.com>
2024-08-08 15:25:08 -0300

    feat: add core files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Graveyard CRUD</title>
+    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./resources/assets/css/style.css">
+</head>
+<body>
+
+    <main>
+        <nav>
+                <img src="./resources/assets/img/logo.png" alt="Logo">
+                <h2>The Graveyard</h2>
+        </nav>
+
+        <section class="banner">
+            <h1>The Graveyard</h1>
+        </section>
+
+        <section class="search">
+            <input type="text" name="search" id="search" placeholder="Search...">
+            <a href="#">Add record</a> <!-- Link to modal | Create -->
+        </section>
+    </main>
+
+</body>
+</html>
